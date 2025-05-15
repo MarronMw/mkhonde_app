21f7e0279c9fffea0316e7480a76a5b4120f6128
ChrispineDzimbiri <chrispinedzimbiri@gmail.com>
2025-05-15 04:06:27 +0200

    imported a package

--- mkhonde_ui/lib/routes.dart
+++ mkhonde_ui/lib/routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mkhonde_ui/screens/group_section_screen.dart';
 import 'screens/language_selection/language_screen.dart';
 import 'package:mkhonde_ui/screens/join_group_screen.dart';
-
+import 'package:mkhonde_ui/screens/group_home_screen.dart';
 // other imports...
 
 Map<String, WidgetBuilder> appRoutes = {
