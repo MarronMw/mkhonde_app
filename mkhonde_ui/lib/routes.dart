c7ead8cd948488460503fdad416923305b98b352
ChrispineDzimbiri <chrispinedzimbiri@gmail.com>
2025-05-15 04:04:45 +0200

    added maingroup app route

--- mkhonde_ui/lib/routes.dart
+++ mkhonde_ui/lib/routes.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:mkhonde_ui/screens/group_section_screen.dart';
 import 'screens/language_selection/language_screen.dart';
 import 'package:mkhonde_ui/screens/join_group_screen.dart';
+
 // other imports...
 
 Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const LanguageScreen(),
   '/group': (context) => const GroupSectionScreen(),
   '/join': (context) => const JoinGroupScreen(),
+  '/maingroup': (context) => const GroupHomeScreen(),
 };
